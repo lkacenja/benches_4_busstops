a1ad82aa48f38146a49780b5163485bd80dfc708
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2022-08-25 20:33:24 -0600

    Basic front end.

--- benches_4_busstops/benches_4_busstops/settings.py
+++ benches_4_busstops/benches_4_busstops/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'backend',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- benches_4_busstops/benches_4_busstops/urls.py
+++ benches_4_busstops/benches_4_busstops/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('backend.urls')),
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
 ]
