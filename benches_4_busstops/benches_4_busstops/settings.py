a428647065f213c294fe0626ea56e033a4694c55
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2022-10-07 16:30:16 -0600

    Use a more functional STATIC_ROOT.

--- benches_4_busstops/benches_4_busstops/settings.py
+++ benches_4_busstops/benches_4_busstops/settings.py
@@ -129,12 +129,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATIC_URL = "/static/"
-
-# Enable WhiteNoise's GZip compression of static assets.
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
