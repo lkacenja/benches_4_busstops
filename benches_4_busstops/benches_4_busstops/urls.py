3b6d86a9a78044339332822a84e9b354e0ff37de
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2022-10-06 20:48:58 -0600

    Add to urls.

--- benches_4_busstops/benches_4_busstops/urls.py
+++ benches_4_busstops/benches_4_busstops/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+import settings
 
 urlpatterns = [
     path('', include('backend.urls')),
     path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
