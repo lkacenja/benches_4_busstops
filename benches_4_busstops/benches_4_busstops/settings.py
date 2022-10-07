f48e98a512273c66a660be56dcb71e0794b1b7db
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2022-10-06 19:59:13 -0600

    Trying STATICFILES_DIRS.

--- benches_4_busstops/benches_4_busstops/settings.py
+++ benches_4_busstops/benches_4_busstops/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
-from .environment import get_env_variable
 import dj_database_url
+import os
+from pathlib import Path
+
+from .environment import get_env_variable
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +134,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
