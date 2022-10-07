070b667a038d2edb2a08ede67649832c7ea71aaf
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2022-10-06 20:54:03 -0600

    Try with debug off.

--- benches_4_busstops/benches_4_busstops/settings.py
+++ benches_4_busstops/benches_4_busstops/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
