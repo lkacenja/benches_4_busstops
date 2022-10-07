1e9d5850d4e997efabc0d8d9586b4cad002bc518
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2022-10-06 20:16:05 -0600

    Copy example settings exactly.

--- benches_4_busstops/benches_4_busstops/settings.py
+++ benches_4_busstops/benches_4_busstops/settings.py
@@ -127,13 +127,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Enable WhiteNoise's GZip compression of static assets.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
