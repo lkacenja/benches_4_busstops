fe386ac6f853b302bf3dc5e84825f50a18f98dbf
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2022-10-06 20:35:25 -0600

    Reverting to previous state

--- benches_4_busstops/benches_4_busstops/settings.py
+++ benches_4_busstops/benches_4_busstops/settings.py
@@ -129,12 +129,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
